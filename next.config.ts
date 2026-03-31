b395c450f4baf377d632b828bbabfefaa9ae815e
萩原涼平 <hagiwara@hagihararyouheinoMacBook-Air.local>
2026-03-31 22:43:25 +0900

    GitHub Pages対応

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: "/official.website",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
